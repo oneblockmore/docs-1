2ee3d85c97ea2923f634d06e93be1bd2075de053
Nick Hynes <nhynes@berkeley.edu>
2019-07-28 10:03:12 -0700

    Fix logo and install build hook

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,23 @@
+const fs = require('fs');
+const promisify = require('util').promisify;
+
+fs.stat('.git/hooks/pre-push', function(err, stats) {
+  if (stats) return;
+  fs.copyFile('.vuepress/pre-push', '.git/hooks/pre-push', function(err) {
+    if (err) {
+      console.error('Could not install docs build hook:', err);
+      process.exit(1);
+    }
+  });
+})
+
 module.exports = {
   title: 'Documentation',
   description: 'Oasis Developer Documentation',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
+  dest: 'docs',
   serviceWorker: true,
   themeConfig: {
     logo: '/logo.png',
@@ -28,5 +42,14 @@ module.exports = {
     docsBranch: 'master',
     editLinks: true,
     editLinkText: 'Help us improve this page!'
+  },
+  chainWebpack(config, isServer) {
+    config
+      .plugin('cname')
+      .use(function() {
+        return function() {
+          fs.writeFile('docs/CNAME', 'docs.oasis.dev\n', function(){});
+        }
+      })
   }
 }
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Welcome to the Oasis Labs developer documentation!
 ## What to do?
 
 * ****NEW\*\*** [Join the Oasis + IDEO + CoinList Hackathon](https://coinlist.co/build/ideo)
-* [Understand the Oasis Platfomr](/overview)
+* [Understand the Oasis Platfom](/overview)
 * [Run through the Quickstart](/quickstart)
 * [Complete the tutorials](/tutorials/ballot)
 * [Watch some Oasis videos](https://www.youtube.com/channel/UC35UFPcZ2F1wjPxhPrSsESQ)
