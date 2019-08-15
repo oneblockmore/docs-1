d08cf9c00eacdc65074296cb7400fc5ae58f4cff
Nick Hynes <nhynes@oasislabs.com>
2019-08-15 14:42:49 -0700

    Remove unneeded files (#31)

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,4 @@
 const fs = require('fs');
-const promisify = require('util').promisify;
-
-fs.stat('.git/hooks/pre-push', function(err, stats) {
-  if (stats) return;
-  fs.copyFile('.vuepress/pre-push', '.git/hooks/pre-push', function(err) {
-    if (err) {
-      console.error('Could not install docs build hook:', err);
-      process.exit(1);
-    }
-  });
-});
 
 module.exports = {
   title: 'Oasis Dev Docs',
@@ -32,7 +21,6 @@ module.exports = {
        gtag('config', 'UA-116576458-2');`,
     ],
   ],
-  dest: 'docs',
   serviceWorker: true,
   themeConfig: {
     logo: '/logo.png',
@@ -62,7 +50,7 @@ module.exports = {
   chainWebpack(config, isServer) {
     config.plugin('cname').use(function() {
       return function() {
-        fs.writeFile('docs/CNAME', 'docs.oasis.dev\n', function() {});
+        fs.writeFile('.vuepress/dist/CNAME', 'docs.oasis.dev\n', function() {});
       };
     });
   },
